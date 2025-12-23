3c265e22654bfa20b05fe5a88e910edf34476087
Gustavo Moraes <gustavo.correiademoraes2006@gmail.com>
2025-12-23 20:33:33 -0300

    feat: initial Spring Boot project setup

---
+++ src/main/java/com/gustavo/jobmonitor/JobMonitorApplication.java
@@ -0,0 +1,13 @@
+package com.gustavo.jobmonitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobMonitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobMonitorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gustavo/jobmonitor/JobMonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gustavo.jobmonitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobMonitorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
